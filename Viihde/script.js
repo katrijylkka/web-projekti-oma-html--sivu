f8d2d9d01a7b8716e61e67a6b5a1c1e0c1ebdcd5
yhimanka <n1ylto00@students.oamk.fi>
2022-04-05 18:38:04 +0300

    kysymykset lisätty

--- Viihde/script.js
+++ Viihde/script.js
@@ -1,46 +1,46 @@
 let questions = [
     {
-        question: "Minkä niminen ilves esiintyi elokuvassa Poika ja Ilves?",
-        optionA: "",
-        optionB: "",
-        optionC: "",
-        optionD: "",
+        question: "Minkä niminen ilves esiintyi elokuvassa Poika ja ilves?",
+        optionA: "Simo",
+        optionB: "Milo",
+        optionC: "Leevi",
+        optionD: "Simba",
         correctOption: "optionC"
     },
 
     {
-        question: "",
-        optionA: "",
-        optionB: "",
-        optionC: "",
-        optionD: "",
+        question: "Mikä eläin oli pääosassa elokuvassa Tunteellinen Niko - Lentäjän poika?",
+        optionA: "Karhu",
+        optionB: "Poro",
+        optionC: "Jänis",
+        optionD: "Hirvi",
         correctOption: "optionB"
     },
 
     {
-        question: "",
-        optionA: "",
-        optionB: "",
-        optionC: "",
-        optionD: "",
+        question: "Minkä nimiset sisarukset näyttelevät Sinikka- ja Tiina Nopolan kirjaan perustuvassa elokuvassa?",
+        optionA: "Lotta ja Suvi",
+        optionB: "Rautahanhi ja Ruohosipuli",
+        optionC: "Sinikka ja Tiina",
+        optionD: "Heinähattu ja Vilttitossu",
         correctOption: "optionD"
     },
 
     {
-        question: "?",
-        optionA: "",
-        optionB: "",
-        optionC: "",
-        optionD: "",
+        question: "Keneltä Onneli ja Anneli ostavat talon ensimmäisessä elokuvassa?",
+        optionA: "Tingelstiina",
+        optionB: "Rouva Rosina Rusina",
+        optionC: "Rouva Ruusupuu",
+        optionD: "Tangelstiina",
         correctOption: "optionC"
     },
 
     {
-        question: "",
-        optionA: "",
-        optionB: "",
-        optionC: "",
-        optionD: "",
+        question: "Minkä tyylinen elokuva on vuonna 2015 ensi-iltansa saanut elokuva Hevisaurus?",
+        optionA: "Komedia",
+        optionB: "Romanttinen",
+        optionC: "Musikaali",
+        optionD: "Seikkailu",
         correctOption: "optionD"
     }
 ]
