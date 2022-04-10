8427d7364728e582168f2903d129ac94fb818cbb
katrijylkkae <katri.jylkka@gmail.com>
2022-04-10 08:42:57 +0300

    luotu 2 nappia, historia.html ja nappien tyylit.

--- historia/historiascript.js
+++ historia/historiascript.js
@@ -1,5 +1,14 @@
 /* Tähän väliin tulee Historia osion Java-script  */
 
+function vastausoikein() {
+    var oikein= document.getAnimations("vastauskenttä").value = "oikein";
+        document.getElementById("vastauskenttä").innerHTML = oikein;
+      }
+    
+      function vastausvaarin() { 
+        var vaarin= document.getElementById("vastauskenttä").value = "väärin";
+        document.getElementById("vastauskenttä").innerHTML = vaarin;
+      }
 
 
 /* Tähän väliin tulee Historia osion Java-script  */
