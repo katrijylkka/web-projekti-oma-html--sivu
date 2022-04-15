4090575a58d4a6990109f040da678ce8f955bc78
katrijylkkae <katri.jylkka@gmail.com>
2022-04-15 14:15:45 +0300

    Historia pelin toiminnallisuuden muokkausta

--- historia/historiascript.js
+++ historia/historiascript.js
@@ -1,59 +1,74 @@
 /* Tähän väliin tulee Historia osion Java-script  */
 
-const ekaPalaute = "Hyvin vedetty!";
-const tokaPalaute = "Nerokasta!";
-const kolmasPalaute = "You go Einstein!";
-const neljäsPalaute = "Fiksu muksu!";
-const viidesPalaute = "Älyköiden aatelia!";
+/*Pelin runko*/
 
-function pelivastaus() {
-let ensimmainenKysymys = document.getElementById("ensimmainenKysymys").value ;
-let toinenKysymys = document.getElementById("toinenKysymys").value ;
-let kolmasKysymys = document.getElementById("kolmasKysymys").value ;
-let neljasKysymys = document.getElementById("neljasKysymys").value ;
-let viidesKysymys = document.getElementById("viidesKysymys").value ;
-let oikein= 0;
+/* Tähän määritelty palautteet */
 
-if (ensimmainenKysymys ==) {
-  oikein++
+const palauteYks= "Vastaathan kysymyksiin, ennen visan palauttamista";
+const palauteKaks= "Pisteet: 1/5     Vaatii hiomista";
+const palauteKolme= "Pisteet: 2/5     Vaatii hiomista";
+const palauteNelja= "Pisteet 3/5     Ihan Jees! Pystytkö petraamaan?";
+const palauteViis = "Pisteet 4/5     Fiksu Muksu!";
+const palauteKuus = "Pisteet 5/5     Todelinen Einstein!";
+
+
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var question5 = document.quiz.question4.value;
+	var correct = 0;
+
+
+	if (question1 == "turku") {
+		correct++;
 }
+	if (question2 == "markka") {
+		correct++;
+}	
+	if (question3 == "svinhufvud ") {
+		correct++;
+	}
+  if (question4 == "tuntematonsotilas") {
+		correct++;
+	}
+  if (question5 == "1917") {
+		correct++;
+	}
+	
+	var pictures = [];
+	var messages = ["Vastaathan kysymyksiin, ennen visan palauttamista","Pisteet: 1/5     Vaatii hiomista","Pisteet: 2/5     Vaatii hiomista", "Pisteet 3/5     Ihan Jees! Pystytkö petraamaan?", "Pisteet 4/5     Fiksu Muksu!","Pisteet 5/5     Todelinen Einstein!"]
+	var score;
 
-if (toinenKysymys ==) {
-  oikein++
+	if (correct = 0) {
+		score = 0;
+	}
+
+	if (correct = 1) {
+		score = 1;
+	}
+
+	if (correct = 2) {
+		score = 2;
+  }  
+
+  if (correct = 3) {
+    score = 3;
+  } 
+
+  if (correct = 4) {
+    score = 4;
+  }   
+
+  if (correct = 5) {
+    score = 5;  
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
 }
-
-if (kolmasKysymys ==) {
-  oikein++
-}
-if (neljasKysymys ==) {
-  oikein++
-}
-if (viidesKysymys ==) {
-  oikein++
-}
-
-var palaute = ["You go Einstein!", "Fiksu muksu, mutta pystyt parempaan", "Kaipaa hiomista!" ]
-var kuvat = [];
-
-let pisteytys;
-
-if (oikein < 1) {
-  pisteytys = 2;
-}  
-
-if ( oikein > 0 && correct < 3) {
-  pisteytys = 1;
-}  
-
-if ( oikein > 2) {
-  pisteytys = 0;
-} 
-
-document.getElementById("vastauksenJalkeen").style.visibility = "visible"
-document.getElementById("numeroOikein").innerHTML = "Vastaus on oikein" + oikein;
-document.getElementById("palaute").innerHTML= palaute [pisteytys]}
-document.getElementById("pisteetOikein").innerHTML = "Pisteet" + oikein ;
-document.getElementById("kuva").src = kuvat [pistetytys]; 
-
-
-/* Tähän väliin tulee Historia osion Java-script  */
