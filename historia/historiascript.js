c9052f7df01a097cf264df63ebcb0b099a2cb6d4
katrijylkkae <katri.jylkka@gmail.com>
2022-04-15 10:07:25 +0300

    Javascript historiascript demo2 luotu. Testaillaan

--- historia/historiascript.js
+++ historia/historiascript.js
@@ -1,14 +1,59 @@
 /* Tähän väliin tulee Historia osion Java-script  */
 
-function vastausoikein() {
-    var oikein= document.getAnimations("vastauskenttä").value = "oikein";
-        document.getElementById("vastauskenttä").innerHTML = oikein;
-      }
-    
-      function vastausvaarin() { 
-        var vaarin= document.getElementById("vastauskenttä").value = "väärin";
-        document.getElementById("vastauskenttä").innerHTML = vaarin;
-      }
+const ekaPalaute = "Hyvin vedetty!";
+const tokaPalaute = "Nerokasta!";
+const kolmasPalaute = "You go Einstein!";
+const neljäsPalaute = "Fiksu muksu!";
+const viidesPalaute = "Älyköiden aatelia!";
+
+function pelivastaus() {
+let ensimmainenKysymys = document.getElementById("ensimmainenKysymys").value ;
+let toinenKysymys = document.getElementById("toinenKysymys").value ;
+let kolmasKysymys = document.getElementById("kolmasKysymys").value ;
+let neljasKysymys = document.getElementById("neljasKysymys").value ;
+let viidesKysymys = document.getElementById("viidesKysymys").value ;
+let oikein= 0;
+
+if (ensimmainenKysymys ==) {
+  oikein++
+}
+
+if (toinenKysymys ==) {
+  oikein++
+}
+
+if (kolmasKysymys ==) {
+  oikein++
+}
+if (neljasKysymys ==) {
+  oikein++
+}
+if (viidesKysymys ==) {
+  oikein++
+}
+
+var palaute = ["You go Einstein!", "Fiksu muksu, mutta pystyt parempaan", "Kaipaa hiomista!" ]
+var kuvat = [];
+
+let pisteytys;
+
+if (oikein < 1) {
+  pisteytys = 2;
+}  
+
+if ( oikein > 0 && correct < 3) {
+  pisteytys = 1;
+}  
+
+if ( oikein > 2) {
+  pisteytys = 0;
+} 
+
+document.getElementById("vastauksenJalkeen").style.visibility = "visible"
+document.getElementById("numeroOikein").innerHTML = "Vastaus on oikein" + oikein;
+document.getElementById("palaute").innerHTML= palaute [pisteytys]}
+document.getElementById("pisteetOikein").innerHTML = "Pisteet" + oikein ;
+document.getElementById("kuva").src = kuvat [pistetytys]; 
 
 
 /* Tähän väliin tulee Historia osion Java-script  */
