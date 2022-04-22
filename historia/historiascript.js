074b4f4c29979d40c1879cb12646233194d832b3
katrijylkkae <katri.jylkka@gmail.com>
2022-04-22 11:56:11 +0300

    Tyylien ja JavaScriptin testailua.

--- historia/historiascript.js
+++ historia/historiascript.js
@@ -1,74 +1,84 @@
 /* Tähän väliin tulee Historia osion Java-script  */
+/*Java-script tiedoston toteuttamiseen lainattu seuraavaa koodia:*/
+/* Kevin Birggs 2016. Youtube-video. Javascript Tutioral: Make a quiz. Hakupäivä:22.4.2022  */ 
 
 /*Pelin runko*/
 
 /* Tähän määritelty palautteet */
 
 const palauteYks= "Vastaathan kysymyksiin, ennen visan palauttamista";
-const palauteKaks= "Pisteet: 1/5     Vaatii hiomista";
-const palauteKolme= "Pisteet: 2/5     Vaatii hiomista";
-const palauteNelja= "Pisteet 3/5     Ihan Jees! Pystytkö petraamaan?";
-const palauteViis = "Pisteet 4/5     Fiksu Muksu!";
-const palauteKuus = "Pisteet 5/5     Todelinen Einstein!";
+const palauteKaks= "Pystyt parempaan!";
+const palauteKolme= "Ihan ok, mutta pystytkö parempaan?";
+const palauteNelja= "Hienoa!";
+const palauteViis = "Loisto suoritus!";
 
+const palauteKuvaYks = "../historia/images/odotan.gif";
+const palauteKuvaKaks = "../historia/images/fail.gif";
+const palauteKuvaKolme = "../historia/images/ok.gif";
+const palauteKuvaNelja = "../historia/images/bravo.gif";
+const palauteKuvaViis = "../historia/images/goodjob.gif";
+
+/* Tässä funktio */ 
+/*muuttujat*/
 
 function check(){
 
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
-  var question4 = document.quiz.question4.value;
-  var question5 = document.quiz.question4.value;
+  	var question4 = document.quiz.question4.value;
+  	var question5 = document.quiz.question4.value;
 	var correct = 0;
 
+// Oikeiden vastausten määrittely
 
-	if (question1 == "turku") {
-		correct++;
+if (question1 == "turku") {
+	correct++;
 }
-	if (question2 == "markka") {
+if (question2 == "markka") {
 		correct++;
 }	
-	if (question3 == "svinhufvud ") {
+if (question3 == "tuntematon") {
 		correct++;
 	}
-  if (question4 == "tuntematonsotilas") {
+if (question4 == "svinhufvud") {
 		correct++;
-	}
-  if (question5 == "1917") {
+}
+if (question5 == "vuosi") {
 		correct++;
-	}
+}
+
+// Pistemäärä määrittää saadun, palautetekstin ja kuvan
 	
-	var pictures = [];
-	var messages = ["Vastaathan kysymyksiin, ennen visan palauttamista","Pisteet: 1/5     Vaatii hiomista","Pisteet: 2/5     Vaatii hiomista", "Pisteet 3/5     Ihan Jees! Pystytkö petraamaan?", "Pisteet 4/5     Fiksu Muksu!","Pisteet 5/5     Todelinen Einstein!"]
-	var score;
+var pictures = [ palauteKuvaYks, palauteKuvaKaks, palauteKuvaKolme, palauteKuvaNelja, palauteKuvaViis ]
+var messages = [palauteYks, palauteKaks, palauteKolme, palauteNelja, palauteViis ]
+var score;
 
-	if (correct = 0) {
-		score = 0;
+	 if (correct == 0) {
+	score = 0;
 	}
 
-	if (correct = 1) {
-		score = 1;
+
+	if (correct > 0 && correct < 3 ) {
+	score = 1;
 	}
+  
 
-	if (correct = 2) {
-		score = 2;
-  }  
+  	if (correct == 3) {
+    score = 2;
+	  }
 
-  if (correct = 3) {
-    score = 3;
-  } 
+	if (correct == 4) {
+		score = 3;
+	}  
 
-  if (correct = 4) {
-    score = 4;
-  }   
+	if (correct == 5) {
+		score == 4; 
+}
 
-  if (correct = 5) {
-    score = 5;  
-	}
-
+// Tulostukset yms.
 	document.getElementById("after_submit").style.visibility = "visible";
-
 	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("number_correct").innerHTML =  correct;
 	document.getElementById("picture").src = pictures[score];
 }
